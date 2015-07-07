799b9843cf477441e163d6fe5f3bc190a4fe6f69
Felipe Wagner <felipew@me.com>
2015-07-07 18:31:39 -0300

    fix(watchVideo): fix the function name on javascript

--- www/rtsplayer.js
+++ www/rtsplayer.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "rtsplayer", "coolMethod", [arg0]);
+exports.watchVideo = function(moviePath, success, error) {
+    exec(success, error, "rtsplayer", "watchVideo", [moviePath]);
 };
